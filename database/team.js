3771d059d7f42da499a660c9fe95ff967dc74df8
anfanger <zhangke.my.186@gmail.com>
2018-11-02 23:33:08 +0800

    基本功能完成

--- database/team.js
+++ database/team.js
@@ -4,37 +4,37 @@
 
 const db = require('sqlongo')('team')
 
-// 队伍信息
+// 队伍信息 (11项，tid,currentPeople,publishedDate为生成项)
 db.team = {
   tid:         'text primary key not null', // 团队唯一id
   masterName:  'text not null',
-  schoolNum:   'text not null',
+  schoolnum:   'text not null',
   qq:          'text not null',
-  teamName:    'text not null',    
-  projectName: 'text not null',   
-  currentPeople:'text not null',            
-  maxPeople:   'int not null',     
-  deadLine:    'text not null',     
-  publishedDate:'text not null',     
-  description: 'text not null' 
+  teamName:    'text not null',
+  projectName: 'text not null',
+  currentPeople:'text not null',
+  maxPeople:   'int not null',
+  deadLine:    'text not null',
+  publishedDate:'text not null',
+  description: 'text not null'
 }
 
 // 对更新时间进行索引
 //db`create index if not exists teamIndex on team(updateTime)`
 
-// 报名信息
+// 报名信息  （11项）
 db.registration = {
   rid:         'text primary key not null', // 报名唯一id
-  tid:         'text not null',    // 报名的队伍id
-  teamName:    'text not null',
-  name:        'text not null',    // 报名人姓名
-  schoolNum:     'text not null',    // 报名人一卡通号
+  tid:         'text not null',             // 报名的队伍id
+  teamName:    'text not null',             // 队伍名称
+  name:        'text not null',             // 报名人姓名
+  schoolnum:     'text not null',           // 报名人学号
   qq:           'text not null',
-  description: 'text not null',    // 报名人自我介绍
-  applicationDate:'text not null',
-  status:      'int not null',     // 状态 0('pending'),1('accepted'),2('rejected'),3('canceled')
-  updateTime:   'int not null',
-  responseText:'text'
+  description: 'text not null',             // 报名人简述理由
+  applicationDate:'text not null',          // 请求发布日期
+  status:      'int not null',      // 状态 0('pending'),1('accepted'),2('rejected'),3('canceled'),4('abandoned')
+  updateTime:   'int not null',             // 更新日期，
+  responseText:'text'               // 目标队伍队长回应信息
 }
 
 module.exports = db
--- routes/api/team/index.js
+++ routes/api/team/index.js
@@ -3,7 +3,7 @@ const db = require('../../../database/team')
 const crypto = require('crypto')
 
 /**
- * team api 竞赛组队API
+ * /api/team 竞赛组队API
  */
 exports.route = {
 
@@ -18,7 +18,7 @@ exports.route = {
     }else if(type==1){
 
       let {schoolNum}=this.user;
-      
+
       let published=await db.team.find(
         {schoolNum},
         pageSize,(page-1)*pageSize,'publishedDate');
@@ -26,7 +26,7 @@ exports.route = {
       let requested=await db.registration.find(
         {schoolNum},
         pageSize,(page-1)*pageSize,'applicationDate');
-      
+
       let tids=[].concat(published).map(eachColume=>eachColume.tid);
 
       let received=await db.registration.find(
@@ -35,16 +35,18 @@ exports.route = {
 
         return{published,requested,received};
     }
-    else{ 
+    else{
      throw 'unknown request type';
     }
   },
-  
+
   async post({ type , data }){
 
+    let selfSchoolNum=this.user.schoolnum;
+
     if(type==1 && Object.keys(data).length==9 ){
-      let {schoolNum,teamName,deadLine} = data;
-      let count=await db.team.count('*',{schoolNum});
+      let {schoolnum,teamName,deadLine} = data;
+      let count=await db.team.count('*',{schoolnum});
 
       if(count==2){
         throw '同时可发布数已达上限(两个)';
@@ -52,30 +54,30 @@ exports.route = {
 
       let tid = crypto.createHash('sha256')
                       .update( teamName )
-                      .update( deadLine ) 
-                      .update( schoolNum )
+                      .update( deadLine )
+                      .update( schoolnum )
                       .digest( 'hex' );
       data.tid=tid;
-      data.currentPeople=schoolNum;
+      data.currentPeople=schoolnum;
       try{
         db.team.insert(data);
         return {status:0}
       }
       catch(e){
-        throw '队伍建立失败';
+        throw '数据库错误';
       }
-      //==================
+      //==================新建申请==========
     }else if(type==2 && Object.keys(data).length==7){
 
       let {tid,applicant,description}=data;
 
-      let pid = crypto.createHash('sha256')
+      let rid = crypto.createHash('sha256')
                       .update( tid )
-                      .update( applicant ) 
+                      .update( applicant )
                       .update( description )
                       .digest( 'hex' );
 
-      data.pid=pid;
+      data.rid=rid;
       data.status=0;
       try{
         db.registration.insert(data);
@@ -84,7 +86,9 @@ exports.route = {
       catch(e){
         throw "commit application failed"
       }
-      //===================
+
+      //===================提交对申请者的回应============
+
     }else if(type==3 && Object.keys(data).length==3){
       let {rid,text,response}=data;
 
@@ -93,13 +97,13 @@ exports.route = {
         throw "reply failed";
       }
       let targetTeam=db.team.find({tid:regisContent.tid});
-      let currentPeople=targetTeam.currentPeople.splite(' ');
-      
+      let currentPeople=targetTeam.currentPeople.split(' ');
+
       if(currentPeople.length>=targetTeam.maxPeople){
         throw "number of alias is up to limit";
       }
       try{
-        let updatedPeople=currentPeople+` ${this.user.schoolNum}`;
+        let updatedPeople=currentPeople+` ${selfSchoolNum}`;
         db.team.update({tid:regisContent.tid},
           {currentPeople:updatedPeople});
 
@@ -111,46 +115,80 @@ exports.route = {
 
       }
       catch(e){
-        throw "database exception"
+        throw "数据库错误"
       }
 
     }else throw 'unknown request type'
   },
 
-  async put({ tid, capacity=0, description='' }) {
+  async put({ type , data }){
 
-    let team = await db.team.find({ tid })
-    let { cardnum } = this.user
-    let updateTime = +moment()
+    let selfSchoolNum=this.user.schoolnum;
 
-    if ( team.organizer === cardnum ) {
-      try {
-        if (capacity) {
-          await db.team.update({ tid }, { capacity, updateTime })
-        }
-        if (description) {
-          await db.team.update({ tid }, { description, updateTime })
-        }
-      } catch(e) {
-        throw '数据库错误'
+    //================修改组队项===========
+    if(type==1 && data.tid){
+      let {tid}=data;
+      let target=db.team.find({tid});
+
+      if(target.schoolnum!==selfSchoolNum)
+        throw 403;
+      if(target.currentPeople.split(' ').length<data.maxPeople)
+        throw 400;
+
+      try{
+        db.team.update({tid},data);
+        return {status:0}
       }
-    } else {
-      throw 403
-    }
-  }, 
-
-  async delete({ tid }) {
-    let team = await db.team.find({ tid })
-    let { cardnum } = this.user
-
-    if ( team.organizer === cardnum ) {
-      try {
-        await db.team.delete({ tid })
-      } catch(e) {
-        throw '数据库错误'
+      catch(e){
+        throw '数据库错误';
       }
-    } else {
-      throw 403
-    }
+      //==================修改申请==========
+    }else if(type==2 && data.rid){
+
+      let {rid}=data;
+      let target=db.registration.find({rid});
+
+      if(target.schoolnum!==selfSchoolNum)
+        throw 403;
+      try{
+
+        db.registration.update({rid},data);
+        return {status:0};
+      }
+      catch(e){
+        throw "数据库错误";
+      }
+    }else throw 'unknown request type'
+  },
+
+  async delete({ type , tid , rid }) {
+    let {schoolnum:selfSchoolNum}=this.user;
+    if(type==1){
+      let team = await db.team.find({ tid });
+      if(selfSchoolNum!==team.schoolnum){
+        throw 403;
+      }
+      try{
+        await db.registration.update({ tid },{status:4});
+        await db.team.delete({tid});
+        return{ status: 0}
+      }
+      catch(e){
+        throw "数据库错误";
+      }
+    }else if(type==2){
+      let regis = await db.registration.find({ rid });
+      if(selfSchoolNum!==regis.schoolnum){
+        throw 403;
+      }
+      try{
+        await db.registration.update({rid},{status:4});
+
+        return{status:0}
+      }
+      catch(e){
+        throw "数据库错误";
+      }
+    }else throw "unknown request type";
   }
 }
